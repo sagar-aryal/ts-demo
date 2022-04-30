9f3169dcf7284c1375756531476b96c6d6602a40
sagar-aryal <sagar_yal92@outlook.com>
2022-04-30 22:35:58 +0300

    bad and good practice of using props with ts

--- src/components/Child.tsx
+++ src/components/Child.tsx
@@ -2,7 +2,13 @@ interface ChildProps {
   color: string;
 }
 
-const Child = ({ color }: ChildProps) => {
+/* const Child = ({ color }: ChildProps) => {
+  return <>{color}</>;
+}; */
+
+/* All react components can optionally provide additional properties like (propsType, displayName, defaultProps and contextTypes). If we use the above convention to define props, TS won't be able to understand that its react component. so the best practice is the below one to define props using ts. */
+
+const Child: React.FC<ChildProps> = ({ color }) => {
   return <>{color}</>;
 };
 
